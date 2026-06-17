39343efecf858bce4ad16342bd8ad5f148ccd4fb
manoelleal <mlpdleal@outlook.com>
2026-06-17 20:30:22 -0300

    fix: ajuste nas rotas de teste

--- test/config/urls.js
+++ test/config/urls.js
@@ -3,13 +3,13 @@ const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 module.exports = {
   BASE_URL,
   PATHS: {
-    LOGIN: "/auth/login",
-    USUARIOS : "/usuarios",
-    PLANOS: "/planos",
-    SUITES: "/suites",
-    CASOS: "/casos",
-    SCRIPTS: "/scripts",
-    PROCEDIMENTOS: "/procedimentos",
-    DEFEITOS: "/defeitos"
+    LOGIN: "/api/auth/login",
+    USUARIOS : "/api/usuarios",
+    PLANOS: "/api/planos",
+    SUITES: "/api/suites",
+    CASOS: "/api/casos",
+    SCRIPTS: "/api/scripts",
+    PROCEDIMENTOS: "/api/procedimentos",
+    DEFEITOS: "/api/defeitos"
   }
 };
